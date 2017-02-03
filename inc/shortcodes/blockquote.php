c85312742f86fd678c4a42bf48105342f38551a0
Marie <marie@violetco.de>
2017-02-02 17:06:54 -0800

    fixes

--- inc/shortcodes/blockquote.php
+++ inc/shortcodes/blockquote.php
@@ -11,6 +11,6 @@
 <blockquote>
   <p><?php echo $text_block; ?></p>
   <?php if ($footer) { ?>
-      <footer><?php echo $footer; ?><cite title="Source Title"><?php echo $source; ?></cite></footer>
+      <footer><?php echo $footer; ?> <cite title="Source Title"><?php echo $source; ?></cite></footer>
   <?php } ?>
 </blockquote>
